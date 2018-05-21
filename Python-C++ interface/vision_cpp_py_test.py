0ee7560d47d7bd48b93652ae7d61b13babaad5ef
Vincent Wen <x.wen@student.tue.nl>
2018-05-21 17:54:17 +0200

    try

--- Python-C++ interface/vision_cpp_py_test.py
+++ Python-C++ interface/vision_cpp_py_test.py
@@ -40,6 +40,6 @@ with open("trajectory.txt", "w") as text_file:
     for row in StateTuples:
         # line = ''.join(str(x) + '  ' for x in row)
         # text_file.write(str(index) + '  ' + line + '\n')
-        text_file.write('{0:4}{1:4}{2:4}{3:4}{4:4}{5:4}{6:4}{7:4}{8:4}{9:4}{10:4}{11:4}{12}\n'.format(index, row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11]))
+        text_file.write('{0:<15}{1[1]:<15}{1[2]:<15}{1[3]:<15}{1[4]:<15}{1[5]:<15}{1[6]:<15}{1[7]:<15}{1[8]:<15}{1[9]:<15}{1[10]:<15}{1[11]:<15}{12:>15}\n'.format(index, row))
         index = index + 1
 
