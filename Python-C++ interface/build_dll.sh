5c8f6876696f37a900a76c7304c10290cdfd26d0
Stijn Ringeling <stijnringeling1@gmail.com>
2018-09-20 16:31:00 +0200

    Updated build scripts
    - Added a local test build script which compiles the old code.
    - Changed the main build script to build new code.

--- Python-C++ interface/build_dll.sh
+++ Python-C++ interface/build_dll.sh
@@ -1,2 +1,2 @@
-g++ -std=c++11 -o libcpp_py_vision.so -shared -fPIC V_PP_module.cpp `pkg-config --cflags --libs opencv` -I /home/osboxes/eigen-git-mirror/
+g++ -std=c++11 -o libcpp_py_vision.so -shared -fPIC src/main.cpp `pkg-config --cflags --libs opencv`
 
---
+++ Python-C++ interface/build_local.sh
@@ -0,0 +1,2 @@
+g++ -std=c++11 -o libcpp_py_vision.so -shared -fPIC V_PP_module.cpp `pkg-config --cflags --libs opencv` -I /home/osboxes/eigen-git-mirror/
+
