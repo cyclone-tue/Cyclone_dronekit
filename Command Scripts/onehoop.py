e11c47c00769fb07715d8b5b04b1f22e9f7fb139
Stijn Ringeling <stijnringeling1@gmail.com>
2018-10-25 13:28:28 +0200

    You can now switch between manual and guided multiple times.

--- Command Scripts/onehoop.py
+++ Command Scripts/onehoop.py
@@ -13,11 +13,16 @@ class flight():
             description='Print out vehicle state information. Connects to SITL on local PC by default.')
         parser.add_argument(
             '--connect', help="vehicle connection target string. If not specified, SITL automatically started and used.")
+        parser.add_argument(
+            '--simulate', help="Include if you want to connect to a simulation in stead of running on an actual drone. Does not wait for guided mode change."
+        )
 
         args = parser.parse_args()
         self.simulation = False
-        if not args.connect:
+        if args.simulate:
             self.simulation = True
+        else:
+            self.simulation = False
         self.path_planning = self.initVision()
         self.drone = self.getDrone(args)
 
@@ -30,9 +35,11 @@ class flight():
             self.drone.awake_script()
         self.drone.obtain_home_location()
         self.drone.set_airspeed(5)
-        
-        remaining_waypoints, frame = self.goToHoop()
-        self.followPath(remaining_waypoints, frame)
+
+        while True:
+            self.goToHoop()
+            if not self.simulation:
+                self.drone.awake_script()
         
 
     def end(self):
@@ -79,12 +86,9 @@ class flight():
         
         points_to_cover = 10        # First n number of points to cover in the trajectory after every recalculation.
 
-
-        path_planning_iteration = True      # Flag for deciding whether to plan the path (if hoop is still in sight).
-
         list_location = []      # Tuples and lists for storing the trajectory information.
         #startPosition = (0, 0, 0) # The position from which the path planning was calculated. All positions from the path are relative to this point.
-        while path_planning_iteration:
+        while self.drone.vehicle.mode.name == "GUIDED":
 
             foundPath = ctypes.c_bool() # is set to true if a path is found, false otherwise
             visualize = ctypes.c_bool(self.simulation) # True if the pathplanning should be visualized using opencv. This can be used for debug purposes.
@@ -97,47 +101,32 @@ class flight():
 
             if foundPath:
                 self.drone.set_home_location()                  # Reset EKF origin for further computations.
-                home_yaw = self.drone.vehicle.attitude.yaw      # Record the heading of the drone after resetting EKF origin.
                 frame = mavutil.mavlink.MAV_FRAME_LOCAL_NED     # Define the frame to use (local NED w.r.t. EKF origin).
-                #startPosition = self.drone.vehicle.location.local_frame
-                #print(startPosition)
                 print("Found path")
                 list_location = []
                 LocationTuples = []
                 for i in range(nrow):
-                    # (x, y, z) waypoints w.r.t. the original position of the drone are parsed at column 0, 4 and 8 of the computed path. North is in the z direction, east is in the x direction and down is in the y direction.
+                    # (x, y, z) waypoints w.r.t. the original position of the drone are
+                    # parsed at column 0, 4 and 8 of the computed path.
+                    # North is in the z direction, east is in the x direction and down is in the y direction.
                     LocationTuples.append((matrix_index(trajectory, ncol, i, 8), matrix_index(
                         trajectory, ncol, i, 0), matrix_index(trajectory, ncol, i, 4)))
-                    # For all the waypoints recoreded, convert them from local NED w.r.t. the heading of the drone to global NED (rotating axes w.r.t. yaw angle).
-                    # list_location.append(self.drone.local_NED_to_global_NED(*LocationTuples[i], yaw=home_yaw))
+                    # For all the waypoints recoreded, convert them from local NED w.r.t.
+                    # the heading of the drone to global NED (rotating axes w.r.t. yaw angle).
                     list_location = LocationTuples
 
 
                 self.followPath(list_location[:points_to_cover], frame)
                 list_location = list_location[points_to_cover:]
             else:
+                # The marker is not currently found
                 print("Could not find path")
                 if len(list_location) > 0:
-                    print("Following previous path and exiting")
+                    # We can folow some parts of the previous parts
+                    print("Following previous path")
                     self.followPath(list_location[:points_to_cover], frame)
                     list_location = list_location[points_to_cover:]
-                    path_planning_iteration = foundPath
-            # fly through first few waypoints
 
-            # check whether hoop is in sight
-            #path_planning_iteration = False           # Flag for checking whether the hoop is still in sight (if not, computed path are all 0).
-            #for item in trajectory:
-            #    if item != 0:
-            #        path_planning_iteration = True
-            #        break
-            #else:
-            #    continue
-
-
-            # extract waypoints from trajectory
-
-      
-        return [list_location, frame]
 
 
     def followPath(self, list_location, frame):
