881792416d140439a39432fc4e067998305a87a0
Xander de Wit <xander.de.wit@xs4all.nl>
2019-05-08 15:40:27 +0200

    yaw in cyclone

--- Command Scripts/cyclone.py
+++ Command Scripts/cyclone.py
@@ -748,6 +748,37 @@ class Cyclone(object):
 
         self.vehicle.send_mavlink(msg)
 
+    def condition_yaw(self, heading, relative=False, duration = 0.0):
+        """
+        Send MAV_CMD_CONDITION_YAW message to point vehicle at a specified heading (in degrees).
+
+        This method sets an absolute heading by default, but you can set the `relative` parameter
+        to `True` to set yaw relative to the current yaw heading.
+
+        By default the yaw of the vehicle will follow the direction of travel. After setting
+        the yaw using this function there is no way to return to the default yaw "follow direction
+        of travel" behaviour (https://github.com/diydrones/ardupilot/issues/2427)
+
+        For more information see:
+        http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_condition_yaw
+        """
+        if relative:
+            is_relative = 1 #yaw relative to direction of travel
+        else:
+            is_relative = 0 #yaw is an absolute angle
+        # create the CONDITION_YAW command using command_long_encode()
+        msg = self.vehicle.message_factory.command_long_encode(
+            0, 0,    # target system, target component
+            mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+            0, #confirmation
+            heading,    # param 1, yaw in degrees
+            0,          # param 2, yaw speed deg/s
+            1,          # param 3, direction -1 ccw, 1 cw
+            is_relative, # param 4, relative offset 1, absolute angle 0
+            0, 0, 0)    # param 5 ~ 7 not used
+        # send command to vehicle
+        self.vehicle.send_mavlink(msg)
+
 
     def set_message_rate(self, period=20000 , message_id=32):
         """
